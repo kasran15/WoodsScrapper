272e4a66f08c4e4291577e8958167f52535257b4
Kasturirangan Parthasarathy <kasturip@lab126.com>
2015-09-27 17:42:51 -0700

    Added caching with Mongo DB

--- news.js
+++ news.js
@@ -9,7 +9,20 @@ var behindwoodsNewsURL = '/tamil-movies-cinema-news-15/',
         'Content-type': 'text/html; charset=utf-8'
     }
     output = '',
-    mysql = require('mysql');
+    mongoose = require('mongoose'),
+    newsSchema = mongoose.Schema({
+        id: String,
+        image: String,
+        article: String
+    }, {
+        collection: 'newsCache'
+    }),
+    News = mongoose.model('News', newsSchema),
+    mongoose.connect('mongodb://bwoods:bwoods@ds051953.mongolab.com:51953/heroku_gv12hs9j', {}, 
+        function() {
+            console.log('connections');
+            console.log(arguments);
+    }); 
 
 exports.getNewsList = function(callback) {
     var newsURL = behindwoodsNewsURL + 'cinema-news.html',
@@ -68,7 +81,6 @@ exports.getNews = function(id, callback) {
 
     output = '',
 
-
     options = {
         hostname: 'www.behindwoods.com',
         path: newsURL,
@@ -77,33 +89,62 @@ exports.getNews = function(id, callback) {
         }
     },
 
-    newsReq = http.request(options, function(newsRes) {
+    receivedNews,
+
+    cachedNews,
+
+    newsReq;
+
+    cachedNews = News.findOne({id: newsURL}, function(err, res) {
+        if (res) {
+            console.log('Cache hit. id:' + newsURL);
+            callback(res.article, res.image);
+            return;
+        }
+
+        newsReq = http.request(options, function(newsRes) {
         
-        newsRes.on('data', function (chunk) {
-            output += chunk;
-        });
-
-        newsRes.on('end', function() {
-            console.log('received the page');
-            jsdom.env(
-                output,
-                ["http://code.jquery.com/jquery.js"],
-                function(errors, window) {
-                    var $img, article, articleImg;
-                    $img = window.$('img[itemprop=\'contentURL\']');
-                    article = window.$('span[itemprop=\'articleBody\']').text(); 
-                    articleImg = $img.attr('src');
-                    callback(article, articleImg);
+            newsRes.on('data', function (chunk) {
+                output += chunk;
             });
+
+            newsRes.on('end', function() {
+                console.log('received the page');
+                jsdom.env(
+                    output,
+                    ["http://code.jquery.com/jquery.js"],
+                    function(errors, window) {
+                        var $img, article, articleImg;
+
+                        $img = window.$('img[itemprop=\'contentURL\']');
+                        article = window.$('span[itemprop=\'articleBody\']').text(); 
+                        articleImg = $img.attr('src');
+
+                        receivedNews = new News({
+                            'id': newsURL,
+                            'article': article,
+                            'image': articleImg
+                        });    
+
+                        receivedNews.save(function(error){
+                            if (error) {
+                                console.log('Error uh-oh ' + error);
+                            }
+                        });
+                        console.log('Triggered a save');
+
+                        callback(article, articleImg);
+                });
+            });
+
+            newsReq.on('error', function(e) {
+                console.log('problem with request: ' + e.message);
+                console.log(e);
+            });
+
+            console.log(id);
+
+            newsReq.end();
         });
     });
-
-    newsReq.on('error', function(e) {
-        console.log('problem with request: ' + e.message);
-        console.log(e);
-    });
-
-    console.log(id);
-
-    newsReq.end();
 };
